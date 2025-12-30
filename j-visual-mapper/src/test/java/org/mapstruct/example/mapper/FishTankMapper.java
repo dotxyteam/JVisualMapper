a81535fabd4a98927a889614fc3c1a1a307c1e50
olitank <olitank@debian>
2025-12-30 19:22:31 +0100

    added workaround for eclipse-maven annotation processing limitation

--- j-visual-mapper/src/test/java/org/mapstruct/example/mapper/FishTankMapper.java
+++ j-visual-mapper/src/test/java/org/mapstruct/example/mapper/FishTankMapper.java
@@ -5,8 +5,6 @@
  */
 package org.mapstruct.example.mapper;
 
-import javax.swing.SwingUtilities;
-
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -14,21 +12,9 @@ import org.mapstruct.example.dto.FishTankDto;
 import org.mapstruct.example.model.FishTank;
 
 import com.otk.jvm.annotation.MappingsResource;
-import xy.reflect.ui.control.swing.renderer.SwingRenderer;
 
 @Mapper
 public interface FishTankMapper {
-
-	public static void main(String[] args) {
-		FishTank source = new FishTank();
-		FishTankDto target = INSTANCE.map(source);
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				SwingRenderer.getDefault().openObjectFrame(target);
-			}
-		});
-	}
 
 	FishTankMapper INSTANCE = com.otk.jvm.util.Mappers.getMapper(FishTankMapper.class,
 			com.otk.jvm.util.Mappers.MAP_STRUCT_FALLBACK_HANDLER);
